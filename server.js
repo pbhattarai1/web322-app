3e90661b5525501bbbdcc8cce6d35880bd0627bb
Rajan Bhattarai <bhattaraipushparaj100@gmail.com>
2023-01-27 13:56:31 -0500

    changing server.js file

--- server.js
+++ server.js
@@ -7,21 +7,25 @@ var HTTP_PORT = process.env.PORT || 8080;
 function onHttpStart() {
   console.log("Express http server listening on: " + HTTP_PORT);
 }
+//// setup another route to listen on /about
+//app.get("/about", function(req,res){
+//res.sendFile(path.join(__dirname,"/views/about.html"));
+//});
 
 // setup a 'route' to listen on the default url path (http://localhost)
-app.get("/about", function (req, res) {
-  res.send(
-    "Pushparaj Bhattarai<br /><a href='/about'>Go to the about page</a>"
-  );
+app.get("/home.html", function (req, res) {
+  res.sendFile(path.join(__dirname, "/views/about.html"));
+  //res.send("Hello World<br /><a href='/about'>Go to the about page</a>");
 });
 
 // setup another route to listen on /about
-app.get("/home", function (req, res) {
-  res.send("<h3>About Pushparaj Bhattarai <br></h3>");
+app.get("/about", function (req, res) {
+  res.send("<h3>About</h3>");
 });
 
 // setup http server to listen on HTTP_PORT
 app.listen(HTTP_PORT, onHttpStart);
+
 // setup the static folder that static resources can load from
 // like images, css files, etc.
-app.use(express.static("public"));
+app.use(express.static("static"));
